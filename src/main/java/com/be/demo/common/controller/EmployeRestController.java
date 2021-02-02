7dee1b8b4a8089d854ffff7f3b415c14d9ad4ac2
bilaleraslan <bilaleraslan@gmail.com>
2021-02-02 13:05:40 +0300

    custom UserDetails kaldırıldı

--- src/main/java/com/be/demo/common/controller/EmployeRestController.java
+++ src/main/java/com/be/demo/common/controller/EmployeRestController.java
@@ -7,6 +7,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,33 +21,35 @@ import com.be.demo.common.services.IEmployeeService;
 @RequestMapping(value = "/api", produces = MediaType.APPLICATION_JSON_VALUE)
 public class EmployeRestController {
 
-	public static final Logger logger = LoggerFactory.getLogger(EmployeRestController.class);
+    public static final Logger logger = LoggerFactory.getLogger(EmployeRestController.class);
 
-	@Autowired
-	private IEmployeeService eService;
+    @Autowired
+    private IEmployeeService eService;
 
-	@GetMapping(value = "/employee-list-all")
-	public ResponseEntity<?> listAll() {
-		List<Employee> list = eService.listAll();
-		return ResponseEntity.ok(list);
-	}
+    @GetMapping(value = "/employee-list-all")
+    public ResponseEntity<?> listAll() {
+	Authentication a = SecurityContextHolder.getContext().getAuthentication();
+	System.out.println(a.getName());
+	List<Employee> list = eService.listAll();
+	return ResponseEntity.ok(list);
+    }
 
-	@GetMapping(value = "/employee-list-by-depId/{depId}")
-	public ResponseEntity<?> listEmployeByDepId(@PathVariable("depId") Integer depId) {
-		List<Employee> list = eService.getAllByDepartmentId(depId);
-		return ResponseEntity.ok(list);
-	}
+    @GetMapping(value = "/employee-list-by-depId/{depId}")
+    public ResponseEntity<?> listEmployeByDepId(@PathVariable("depId") Integer depId) {
+	List<Employee> list = eService.getAllByDepartmentId(depId);
+	return ResponseEntity.ok(list);
+    }
 
-	@GetMapping(value = "/employee-list-all-name")
-	public ResponseEntity<?> listAllEmployeeNames() {
-		List<String> list = eService.getAllNames();
-		return ResponseEntity.ok(list);
-	}
+    @GetMapping(value = "/employee-list-all-name")
+    public ResponseEntity<?> listAllEmployeeNames() {
+	List<String> list = eService.getAllNames();
+	return ResponseEntity.ok(list);
+    }
 
-	@GetMapping(value = "/employee-list-by-name/{name}")
-	public ResponseEntity<?> listEmployeByName(@PathVariable("name") String name) {
-		List<Employee> list = eService.getByFirstName(name);
-		return ResponseEntity.ok(list);
-	}
+    @GetMapping(value = "/employee-list-by-name/{name}")
+    public ResponseEntity<?> listEmployeByName(@PathVariable("name") String name) {
+	List<Employee> list = eService.getByFirstName(name);
+	return ResponseEntity.ok(list);
+    }
 
 }
